894ed75fcbbba95bf8dec8aaa92ef3e128141b98
kim.lee@tqcsulotion.com <kim.lee@tqcsulotion.com>
2024-11-19 16:53:56 +0700

    feat: test CI/CD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,8 +16,8 @@ export default function Home() {
         <pre className="text-lg bg-slate-400">
           To generate this message, Docker took the following steps: 1. The
           Docker client contacted the Docker daemon. 2. The Docker daemon pulled
-          the "hello-world" image from the Docker Hub. (amd64) 3. The Docker
-          daemon created a new container from that image which runs the
+          the &quot;hello-world&quot; image from the Docker Hub. (amd64) 3. The
+          Docker daemon created a new container from that image which runs the
           executable that produces the output you are currently reading. 4. The
           Docker daemon streamed that output to the Docker client, which sent it
           to your terminal.
